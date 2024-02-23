2fd8af008910b69abd40f4847df4726915f882c3
Barrett Anderson <barrett1614@gmail.com>
2024-02-23 14:34:41 -0600

    Finished 'Creating Your Server'

--- server/index.js
+++ server/index.js
@@ -3,7 +3,28 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 4004;
 
+// See https://ed.devmountain.com/materials/ahweb11/units/react-proj-4/
+
 app.use(express.json());
 app.use(cors());
 
 app.listen(PORT, () => console.log(`Running on Port ${PORT}`));
+
+const {
+  getAllPosts,
+  getCurrentUserPosts,
+  addPost,
+  editPost,
+  deletePost,
+} = require("./controllers/posts");
+const { register, login } = require("./controllers/auth");
+
+app.post("/register", register);
+app.post("/login", login);
+
+app.get("/posts", getAllPosts);
+
+app.get("/userposts/:userId", getCurrentUserPosts);
+app.post("/posts", addPost);
+app.put("/posts/:id", editPost);
+app.delete("/posts/:id", deletePost);
