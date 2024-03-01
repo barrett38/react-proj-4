5830d9b3abf52ee9c1dcc3735109e3bc4472ab0f
Barrett Anderson <barrett1614@gmail.com>
2024-03-01 09:18:29 -0600

    Completed Creating a Database

--- server/index.js
+++ server/index.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 4004;
-
-// See https://ed.devmountain.com/materials/ahweb11/units/react-proj-4/
-
-app.use(express.json());
-app.use(cors());
-
-app.listen(PORT, () => console.log(`Running on Port ${PORT}`));
+const PORT = process.env.PORT || 4005;
+const { sequelize } = require("./util/database");
+const { User } = require("./models/user");
+const { Post } = require("./models/post");
 
 const {
   getAllPosts,
@@ -18,6 +14,13 @@ const {
   deletePost,
 } = require("./controllers/posts");
 const { register, login } = require("./controllers/auth");
+const { isAuthenticated } = require("./middleware/isAuthenticated");
+
+app.use(express.json());
+app.use(cors());
+
+User.hasMany(Post);
+Post.belongsTo(User);
 
 app.post("/register", register);
 app.post("/login", login);
@@ -28,3 +31,15 @@ app.get("/userposts/:userId", getCurrentUserPosts);
 app.post("/posts", isAuthenticated, addPost);
 app.put("/posts/:id", isAuthenticated, editPost);
 app.delete("/posts/:id", isAuthenticated, deletePost);
+
+// the force: true is for development -- it DROPS tables!!!
+// you can use it if you like while you are building.
+// sequelize.sync({ force: true })
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`db sync successful & server running on port ${PORT}`)
+    );
+  })
+  .catch((err) => console.log(err));
